42466079523c5b948a02367fccbee899ed85e112
Glori Jenn Dangan <daryjedbdanganictb12@gmail.com>
2024-02-01 10:44:54 +0800

    innitial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>  
+import {ref} from'vue';
+let counter = ref(0);
+
+function add(){
+  counter.value++;
+}
+  function sub(){
+  counter.value--;
+}
+
+</script>
+
+<template>
+<div>
+  <h1>{{counter}}</h1> 
+      <button @click="add()"> + </button>
+     <button @click="sub()"> - </button>
+   </div>
+  </template>
+  <style>
+  body{
+    text-align: center;  
+  }
+  </style>
